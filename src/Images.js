8120d3465c62668e6dd3d50a83b7c2850a486952
Richard Ali <richardali@richards-mbp.lan>
2019-03-25 22:33:17 -0700

    use friendly names

--- src/Images.js
+++ src/Images.js
@@ -1,35 +1,35 @@
 export const images = [{
     id: 1,
     name: "1",
-    image: "IMG_0053.jpg"
+    image: "goldengate_1.jpg"
 }, {
     id: 2,
     name: "2",
-    image: "IMG_0112.jpg"
+    image: "caltrain_1.jpg"
 }, {
     id: 3,
     name: "3",
-    image: "IMG_0320.jpg"
+    image: "brooklyn_1.jpg"
 }, {
     id: 4,
     name: "4",
-    image: "IMG_0573.jpg"
+    image: "alcatraz_1.jpg"
 }, {
     id: 5,
     name: "5",
-    image: "IMG_2118.jpg"
+    image: "fourbarrel_1.jpg"
 }, {
     id: 6,
     name: "6",
-    image: "IMG_2156.jpg"
+    image: "cinebar_1.jpg"
 }, {
     id: 7,
     name: "7",
-    image: "IMG_2324.jpg"
+    image: "goldengatepark_1.jpg"
 }, {
     id: 8,
     name: "8",
-    image: "IMG_3280.jpg"
+    image: "trolley_1.jpg"
 }];
 
 export default {
